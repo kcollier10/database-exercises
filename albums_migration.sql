c43951ea8acc4c9635e4047a6d7504449be0fd81
kcollier10 <kristencollier10@gmail.com>
2021-01-26 14:08:43 -0600

    users, databases, tables

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,7 +1,6 @@
 USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
-
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL DEFAULT 'unknown',
@@ -10,5 +9,6 @@ CREATE TABLE albums (
     sales FLOAT UNSIGNED,
     genre VARCHAR(100),
     PRIMARY KEY(id)
-)
+);
+
 INSERT INTO albums (artist, name, release_date, sales, genre) VALUES ('Band name', 'Album', 47529, 3284329, 'Alternative Rock');
