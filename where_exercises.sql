dff6471d74cdab966cb20489ca14113c5d8b2d43
kcollier10 <kristencollier10@gmail.com>
2021-01-26 15:39:33 -0600

    where exercises

---
+++ where_exercises.sql
@@ -0,0 +1,62 @@
+USE employees;
+
+# Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+# Find all employees whose last name starts with 'E' — 7,330 rows.
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%';
+
+# Find all employees hired in the 90s — 135,214 rows.
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+# Find all employees born on Christmas — 842 rows.
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%12-25';
+
+# Find all employees with a 'q' in their last name — 1,873 rows.
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+    OR last_name LIKE '%Q%';
+
+# ----------------------------------------
+
+# Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
+SELECT first_name, last_name
+FROM employees
+WHERE gender like 'M'
+    AND (first_name LIKE 'Irena'
+        OR first_name LIKE 'Vidya'
+        OR first_name LIKE 'Maya');
+# Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
+
+# Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%E'
+    OR last_name LIKE 'E%';
+
+# Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%E'
+    AND last_name LIKE 'E%';
+
+# Find all employees hired in the 90s and born on Christmas — 362 rows.
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%12-25'
+    AND hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+# Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%q%';
