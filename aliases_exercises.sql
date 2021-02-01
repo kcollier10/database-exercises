3d9b3fd3dd669efd14c964a96c0e77ccc1f5d114
kcollier10 <kristencollier10@gmail.com>
2021-01-31 21:38:15 -0600

    updates

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -3,7 +3,7 @@ USE employees;
 # Return 10 employees in a result set named 'full_name' in the format of 'last name, first name' for each employee.
 SELECT CONCAT(first_name, ' ', last_name) AS full_name
 FROM employees
-LIMIT 5;
+LIMIT 7;
 
 #Add the date of birth for each employee as 'DOB' to the query.
 SELECT CONCAT(first_name, ' ', last_name) AS name, birth_date as DOB
--- limit_exercises.sql
+++ limit_exercises.sql
@@ -4,7 +4,7 @@ SELECT DISTINCT last_name FROM employees
 ORDER BY last_name DESC
 LIMIT 10;
 
-SELECT first_name, last_name
+SELECT last_name, first_name
 FROM employees
 WHERE birth_date LIKE '%12-25'
   AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
