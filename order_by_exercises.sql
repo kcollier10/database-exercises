87b04fa07eb4d82741c3313bdd7831a2699893a5
kcollier10 <kristencollier10@gmail.com>
2021-02-17 22:29:17 -0600

    feat: messing with queries

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -114,4 +114,5 @@ SELECT first_name, last_name
 FROM employees
 WHERE birth_date LIKE '%12-25'
     AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-ORDER BY birth_date ASC, hire_date DESC;
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 10;
