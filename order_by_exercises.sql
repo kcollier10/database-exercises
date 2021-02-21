727b602aee2d4e4b0f1e743c8a45c7a6689ceb20
kcollier10 <kristencollier10@gmail.com>
2021-02-20 22:58:07 -0600

    feat: practicing SQL queries

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -116,3 +116,8 @@ WHERE birth_date LIKE '%12-25'
     AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
 ORDER BY birth_date ASC, hire_date DESC
 LIMIT 5;
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%10-31'
+ORDER BY birth_date DESC;
